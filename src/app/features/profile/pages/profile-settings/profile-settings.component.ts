81d0ad78488eb08776be16676d68af7f9037578d
juankkay010 <juanandrescuervoaguirre8@gmail.com>
2024-10-05 00:20:09 -0500

    profile picture done

--- src/app/features/profile/pages/profile-settings/profile-settings.component.ts
+++ src/app/features/profile/pages/profile-settings/profile-settings.component.ts
@@ -8,6 +8,9 @@ import { SignalUser } from '../../../../auth/interfaces/signal-user';
 import { ProfileService } from '../../services/profile.service';
 import Swal from 'sweetalert2';
 import { HttpClientModule } from '@angular/common/http';
+import { SupabaseService } from '../../../properties/services/supabase.service';
+import {v4 as uuidv4} from 'uuid';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -16,7 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-profile-settings',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, RouterModule,HttpClientModule],
+  imports: [FormsModule, ReactiveFormsModule, RouterModule,HttpClientModule, CommonModule],
   providers: [ProfileService],
   templateUrl: './profile-settings.component.html',
   styleUrls: ['./profile-settings.component.css']
@@ -27,9 +30,14 @@ export class ProfileSettingsComponent {
 
   editProfileForm:FormGroup;
   userSignal!: Signal<SignalUser>
-  profileServices
+  profileServices;
+  selectedFile: File | null = null;
+  imagePreview: string | ArrayBuffer | null = null;
 
-  constructor(private fb: FormBuilder, private router: Router,private userService: UserService,private profileService:ProfileService) {
+
+  constructor(private fb: FormBuilder, private router: Router,private userService: UserService,private profileService:ProfileService,
+    private supabaseService: SupabaseService
+  ) {
 
     this.userSignal = this.userService.userSignal;
     this.profileServices = this.profileService
@@ -40,29 +48,22 @@ export class ProfileSettingsComponent {
     email: [''],
     password: [''],
     bio: [''],
-    profile_picture: [''],
   });
 
 }
-  profile = {
-    img: 'path/to/image.jpg',
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'john.doe@example.com'
-  };
-
-  saveProfile() {
-    console.log('Profile saved', this.profile);
-  }
 
 
-  onEditProfile(){
+  async onEditProfile(){
 
     let userName = this.editProfileForm.value.userName||'';
     let password = this.editProfileForm.value.password||''; 
     let email = this.editProfileForm.value.email||'';
     let bio = this.editProfileForm.value.bio||'';
-    let profile_picture = this.editProfileForm.value.profile_picture||'';
+    let profile_picture = '';
+
+    if(this.selectedFile){
+      profile_picture = await this.uploadProfileImage();
+    }
 
 
 
@@ -79,13 +80,39 @@ export class ProfileSettingsComponent {
     }
 
 
+
     
 
-
-
-    console.log(userName,password,email,bio,profile_picture)
-    console.log(this.userSignal())
   }
+
+  async uploadProfileImage(): Promise<any>{
+    if(!this.selectedFile)return null;
+    const fileName = `${uuidv4()}.${this.selectedFile.name.split('.').pop()}`;
+    const foldername = 'profile_images';
+
+    try{
+      const publicUrl = await this.supabaseService.upload(this.selectedFile, fileName, foldername);
+      return publicUrl;
+    } catch(error){
+      console.error('Error subiendo la imagen:', error);
+      return null;
+    }
+  }
+
+  onFileSelected(event: Event){
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]){
+      this.selectedFile = input.files[0];
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagePreview = reader.result as string;
+      };
+
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+
 
 
 
